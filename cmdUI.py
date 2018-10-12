388e6c540f7fed56d9ec7e17724c43264c48f854
Austin Stover <stover.a@wustl.edu>
2018-10-12 18:25:33 -0500

    Fixed and improved cmdUI.py.
    TODO: Add "all" option for commands

--- cmdUI.py
+++ cmdUI.py
@@ -7,28 +7,31 @@ This program institutes the command line interface for the DacManager programs,
 as well as for a temperature plate.
 
 Use: Run python cmdUI.py
-    -h for man
-    On Windows, run cmd.exe as administrator
+     -h for man
+     On Windows, run cmd.exe as administrator
 
-    TODO: Add "all" to cmd line addresses
+     TODO: Add "all" to cmd line addresses
 """
 
 from DacMaster import DacMaster as dm
 import argparse
 import pickle
+import os
 
 ### DEFAULTS
 
-COM_PORT = 'COM4'
+COM_PORT = '/dev/ttyUSB0' # Winows com port: 'COM4'  Raspbian Com port: '/dev/ttyUSB0'
 DAC_DIR_FILE = 'DacDir.txt' #Dac Directory File
 
 ### HANDLE PERSISTING VARIABLES
 
-VAR_FILE_DIR = '/'
+VAR_FILE_DIR = ''
 VAR_FILE_NAME = 'cmd_UI_vars.obj' #Should have extension '.obj' in it
 
+varFileAbsDir = os.path.join(os.path.dirname(__file__),VAR_FILE_DIR,VAR_FILE_NAME) #Absolute directory
+
 def init_command(): #Unpack vars from file and reinitialize cntrl object
-    persisting_vars = pickle.load(open(VAR_FILE_DIR + VAR_FILE_NAME, 'rb')) #open() only works with admin priveleges
+    persisting_vars = pickle.load(open(varFileAbsDir, 'rb')) #open() only works with admin priveleges
     print(*persisting_vars[:-1])
     cntrl = dm(*persisting_vars[:-1])
     addressDict = persisting_vars[-1]
@@ -59,7 +62,7 @@ def init(args):
     
     persisting_vars = (args.slaveId, args.port, args.baudrate, args.numBoards, addressDict) #A tuple of vars to store in between commands
     
-    var_file = open(VAR_FILE_DIR + VAR_FILE_NAME, 'wb')
+    var_file = open(varFileAbsDir, 'wb')
     pickle.dump(persisting_vars,var_file) #Save persisting variables in obj file
             
 
@@ -105,11 +108,13 @@ def powDown(args):
         print(channel)
         cntrl.powerDown(channel)
     except KeyError:
-        print('Error: DAC channel address not found')
+        print('Error: DAC channel address not found. See DacDir.txt or the ' +
+              'DAC directory file you specified for a list of available DAC ' +
+              'names.')
         exit(1)
         
 psr_powDown = subpsrs.add_parser('powerDown', aliases=['powDn','powDown'],
-                                    help='Power down a channel')
+                                  help='Power down a channel')
 psr_powDown.add_argument('chan', type=str, 
                           help="Address of DAC channel")
 psr_powDown.set_defaults(func=powDown)
@@ -128,7 +133,7 @@ def getV(args):
 psr_getV = subpsrs.add_parser('getV', aliases=['gtV'],
                                help='Returns the voltage for the DAC channel')
 psr_getV.add_argument('chan', type=str, 
-                          help="Address of DAC channel")
+                       help="Address of DAC channel")
 psr_getV.set_defaults(func=getV)
 
 #updateV command
@@ -152,4 +157,9 @@ psr_updateV.set_defaults(func=updateV)
 
 ### PARSE COMMAND LINE AND EVALUATE
 args = psr.parse_args() #Parse the args
-args.func(args)         #Call whatever function was selected
+
+if(bool(vars(args))): #Check if command argument supplied
+    args.func(args)         #Call whatever function was selected
+else:
+    raise ValueError('A valid command or option is required to run this script. ' +
+                     'Use the option \'-h\' for a list of valid commands and options.')
